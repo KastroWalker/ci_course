5c502bcbf30a7e91f9efe571b75dfeea99a93802
Victor <victor.araujo@grupoboticario.com.br>
2022-06-04 09:51:26 -0300

    Adiciona teste falhando para testar gh action

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ const app = express();
 
 app.use(cors());
 
+app.get("/", (_req, res) => {
+  res.status(200).send("Hello GH actions!");
+});
+
 app.get("/test", (_req, res) => {
   res.status(200).send("Hello world");
 });
+
 module.exports = app;
--- src/app.test.js
+++ src/app.test.js
@@ -2,6 +2,14 @@ const app = require("./app");
 const supertest = require("supertest");
 const request = supertest(app);
 
+describe("/ endpoint", () => {
+  it("should return 200", async () => {
+    const response = await request.get("/");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Invalid message!");
+  });
+});
+
 describe("/test endpoint", () => {
   it("should return a response", async () => {
     const response = await request.get("/test");
