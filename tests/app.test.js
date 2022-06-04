b6acabb4589434ef4eef7ccb83f29014f56fee43
Victor <victor.araujo@grupoboticario.com.br>
2022-06-04 11:07:32 -0300

    Adiciona coverage baixo

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,13 +2,13 @@ const app = require("../src/app");
 const supertest = require("supertest");
 const request = supertest(app);
 
-describe("/ endpoint", () => {
-  it("should return 200", async () => {
-    const response = await request.get("/");
-    expect(response.status).toBe(200);
-    expect(response.text).toBe("Hello GH actions!");
-  });
-});
+// describe("/ endpoint", () => {
+//   it("should return 200", async () => {
+//     const response = await request.get("/");
+//     expect(response.status).toBe(200);
+//     expect(response.text).toBe("Hello GH actions!");
+//   });
+// });
 
 describe("/students endpoint", () => {
   it("should return 5 students", async () => {
